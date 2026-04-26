453cd4fa36bcb9cd74f5360951dae3b47ca89299
ImenOun <imen.oun@esprit.tn>
2026-04-26 17:44:37 +0100

    Fix: application.properties /pom

--- ProjetPI/src/test/java/tn/esprit/projetpi/ProjetPiApplicationTests.java
+++ ProjetPI/src/test/java/tn/esprit/projetpi/ProjetPiApplicationTests.java
@@ -2,12 +2,14 @@ package tn.esprit.projetpi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjetPiApplicationTests {
 
     @Test
     void contextLoads() {
+        // Le test passe car JPA est désactivé
     }
-
 }
