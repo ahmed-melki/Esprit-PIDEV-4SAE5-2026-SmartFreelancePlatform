7c7fba788095e2124618b4d7c38fdb000d2de065
ImenOun <imen.oun@esprit.tn>
2026-05-02 19:30:27 +0100

    modiffffff5

--- JobOffre/src/test/java/tn/esprit/joboffre/JobOffreApplicationTests.java
+++ JobOffre/src/test/java/tn/esprit/joboffre/JobOffreApplicationTests.java
@@ -1,19 +1,6 @@
 package tn.esprit.joboffre;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
 @SpringBootTest
-@Disabled("Test désactivé pour la CI - les tests unitaires suffisent")
-class JobOffreApplicationTests {
-
-	
-    @Test
-    @Disabled
-    void contextLoads() {
-        
-    }
-
+class JobOffreApplicationTests{
+//supprimer
 }
